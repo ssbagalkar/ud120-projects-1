d24f090cb01222be493665f71b016cb2adbb19e2
ssbagalkar <sbagalka@asu.edu>
2016-09-18 15:13:20 -0500

    SVM code

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(kernel = 'linear')
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time :",round(time()-t0,3),"s"
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time :",round(time()-t1,3),"s"
+
+from sklearn.metrics import accuracy_score
+score = accuracy_score (labels_test,pred)
+print "The accuracy of SVM model is:",score
+
+
+
+
 
 #########################################################
 
